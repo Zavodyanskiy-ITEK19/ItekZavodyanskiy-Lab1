d2d83fe8897592bc44726e789caf17401aea602e
Zavodyanskiy-ITEK19 <fedor.z849@gmail.com>
2021-04-12 13:55:36 +0300

    ItekZavodyanskiy-Lab1

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Введитечисло ");
+            int k = Int32.Parse(Console.ReadLine());
+            if (k % 2 == 0)
+            {
+                Console.WriteLine("числочетное\n");
+            }
+            else
+            {
+                Console.WriteLine(" числонечетное\n");
+            }
+
+        }
+    }
+}
